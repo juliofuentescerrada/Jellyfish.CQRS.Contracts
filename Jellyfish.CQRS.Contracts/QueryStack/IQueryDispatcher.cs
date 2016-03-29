546cfc4253d6c547dbb39574d917a4ee1f025f93
juliofuentescerrada <julio.fuentes.cerrada@outlook.com>
2016-03-29 20:47:04 +0200

    Stylecop: Move using inside of the namespace

--- Jellyfish.CQRS.Contracts/QueryStack/IQueryDispatcher.cs
+++ Jellyfish.CQRS.Contracts/QueryStack/IQueryDispatcher.cs
@@ -1,7 +1,7 @@
-﻿using System.Threading.Tasks;
-
-namespace Jellyfish.CQRS.Contracts.QueryStack
+﻿namespace Jellyfish.CQRS.Contracts.QueryStack
 {
+    using System.Threading.Tasks;
+
     public interface IQueryDispatcher
     {
         Task<TResult> Dispatch<TResult, TQuery>(TQuery query) where TQuery : IQuery where TResult : IQueryResult;
